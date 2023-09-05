329f05542303d3880f51ee53d863b8026a2e385b
munseonBaek <munseon01@naver.com>
2023-09-05 20:57:10 +0900

    Create 문제1001.java

---
+++ 문제1001.java
@@ -0,0 +1,19 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 문제1001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("");
+		int a = sc.nextInt();
+		System.out.print("");
+		int b = sc.nextInt();
+		
+		System.out.println(a-b);
+	}
+
+}
