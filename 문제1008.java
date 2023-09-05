f01f6b1dd4ae53caae8f6a6a4a4eeb9daee4699f
munseonBaek <munseon01@naver.com>
2023-09-05 20:57:11 +0900

    Create 문제1008.java

---
+++ 문제1008.java
@@ -0,0 +1,19 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 문제1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("");
+		double a = sc.nextDouble();
+		System.out.print("");
+		double b = sc.nextDouble();
+		
+		System.out.println(a/b);
+	}
+
+}
