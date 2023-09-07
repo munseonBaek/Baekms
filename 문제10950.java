aaa2a07476eb57dbbddbc2b1197f3141ed44fc01
munseonBaek <munseon01@naver.com>
2023-09-07 19:10:09 +0900

    Create 문제10950.java

---
+++ 문제10950.java
@@ -0,0 +1,21 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class 문제10950 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for (int i=0; i<a; i++) {
+			int l = sc.nextInt();
+			int r = sc.nextInt();
+			
+			System.out.println(l+r);
+		}
+	}
+
+}
