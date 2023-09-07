b9c09b0e58450f9f761c1899b286895c79d126a8
munseonBaek <munseon01@naver.com>
2023-09-07 19:10:10 +0900

    Create 문제2739.java

---
+++ 문제2739.java
@@ -0,0 +1,18 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class 문제2739 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(a+" * "+i+" = "+a*i);
+		}
+	}
+
+}
