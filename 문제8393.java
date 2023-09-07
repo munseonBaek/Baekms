91d33417eaa5215c80f82e8a77ad5abe9dd2bbee
munseonBaek <munseon01@naver.com>
2023-09-07 19:10:13 +0900

    Create 문제8393.java

---
+++ 문제8393.java
@@ -0,0 +1,20 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class 문제8393 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int sum=0;
+		
+		for (int i=0; i<=a; i++) {
+			sum+=i;
+			
+		}System.out.println(sum);
+	}
+
+}
