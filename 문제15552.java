6a99309068dd2aecdcb093221ea4a5343c5a4959
munseonBaek <munseon01@naver.com>
2023-09-09 22:33:46 +0900

    Create 문제15552.java

---
+++ 문제15552.java
@@ -0,0 +1,21 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class 문제15552 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for (int i=0; i<t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			System.out.println(a+b);
+		}
+	}
+
+}
