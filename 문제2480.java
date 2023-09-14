4bb079bcf03cda4d20af10ff681f40cfa7126f14
munseonBaek <munseon01@naver.com>
2023-09-14 20:14:13 +0900

    Create 문제2480.java

---
+++ 문제2480.java
@@ -0,0 +1,41 @@
+package 조건문;
+
+import java.util.Scanner;
+
+public class 문제2480 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		if (a == b && b == c && a == c) {
+			System.out.println(10000 + a * 1000);
+		} else if (a == b && b != c) {
+			System.out.println(1000 + a * 100);
+		} else if (b == c && c != a) {
+			System.out.println(1000 + b * 100);
+		} else if (a == c && a != b) {
+			System.out.println(1000 + c * 100);
+		} else {
+			if (a > b) {
+				if (a > c) {
+					System.out.println(a * 100);
+				} else {
+					System.out.println(c * 100);
+				}
+			}
+			if (b > a) {
+				if (b > c) {
+					System.out.println(b * 100);
+				} else {
+					System.out.println(c * 100);
+				}
+			}
+		}
+	}
+
+}
