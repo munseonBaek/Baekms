e4fba07e7025f45515f576be38fcdf00683f9682
munseonBaek <munseon01@naver.com>
2023-09-14 20:14:10 +0900

    Create 문제1330.java

---
+++ 문제1330.java
@@ -0,0 +1,25 @@
+package 조건문;
+
+import java.util.Scanner;
+
+public class 문제1330 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if (a==b) {
+			System.out.println("==");
+		} else if (a>b){
+			System.out.println(">");
+		} else if (a<b){
+			System.out.println("<");
+		
+		}
+		
+	}
+
+}
