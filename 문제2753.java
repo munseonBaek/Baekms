a50da582f1241e2df79a2e2eaa4113ef4e0ae5a9
munseonBaek <munseon01@naver.com>
2023-09-14 20:14:16 +0900

    Create 문제2753.java

---
+++ 문제2753.java
@@ -0,0 +1,20 @@
+package 조건문;
+
+import java.util.Scanner;
+
+public class 문제2753 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		if (a%4==0 && a%100!=0 || a%400==0) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+	}
+
+}
