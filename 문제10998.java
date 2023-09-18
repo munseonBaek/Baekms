f41c9e993710178742cdb183112725a0049ddbb9
munseonBaek <munseon01@naver.com>
2023-09-18 23:12:49 +0900

    Create 문제10998.java

---
+++ 문제10998.java
@@ -0,0 +1,21 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 문제10998 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("");
+		int a = sc.nextInt();
+		System.out.print("");
+		int b = sc.nextInt();
+		
+		System.out.println(a*b);
+		
+	}
+
+}
