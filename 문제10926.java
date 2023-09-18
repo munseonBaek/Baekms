b72b808e3367f30398ea2d5ab1cd4d50a4875b9a
munseonBaek <munseon01@naver.com>
2023-09-18 23:12:35 +0900

    Create 문제10926.java

---
+++ 문제10926.java
@@ -0,0 +1,19 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 문제10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+
+		String a = sc.next();
+		
+		System.out.println(a+"??!");
+	}
+
+}
