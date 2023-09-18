45d2b82b99c1091fb225fe238e3eafa5610743fd
munseonBaek <munseon01@naver.com>
2023-09-18 23:12:40 +0900

    Create 문제18108.java

---
+++ 문제18108.java
@@ -0,0 +1,16 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 문제18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		System.out.println(a-543);
+	}
+
+}
