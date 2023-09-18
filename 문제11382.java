a25eed4a3a4850b422facc017945de8bf7b71721
munseonBaek <munseon01@naver.com>
2023-09-18 23:12:44 +0900

    Create 문제11382.java

---
+++ 문제11382.java
@@ -0,0 +1,18 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class 문제11382 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		
+		System.out.println(a+b+c);
+	}
+
+}
