3c50b7730f9c0f2a4cfd7860c5528f752c86fc23
munseonBaek <munseon01@naver.com>
2023-09-23 23:38:39 +0900

    Create 문제10171.java

---
+++ 문제10171.java
@@ -0,0 +1,14 @@
+package 입출력과사칙연산;
+
+public class 문제10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
