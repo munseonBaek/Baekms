7c293653da8eb5d2f64b1d612033ac97811f2247
munseonBaek <munseon01@naver.com>
2023-09-27 12:51:00 +0900

    Create FavoriteBean.java

---
+++ FavoriteBean.java
@@ -0,0 +1,28 @@
+package ch04;
+
+public class FavoriteBean {
+	String color;
+	String flower;
+	String music;
+	
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getFlower() {
+		return flower;
+	}
+	public void setFlower(String flower) {
+		this.flower = flower;
+	}
+	public String getMusic() {
+		return music;
+	}
+	public void setMusic(String music) {
+		this.music = music;
+	}
+	
+}
