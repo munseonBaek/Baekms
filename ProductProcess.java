e3ef63e88b026fd493099ed1df0d8c2e6da7f1ef
munseonBaek <munseon01@naver.com>
2023-09-29 14:43:44 +0900

    Create ProductProcess.java

---
+++ ProductProcess.java
@@ -0,0 +1,63 @@
+package ch04;
+
+public class ProductProcess {
+	String productid;		//상품 코드
+	String name;			//상품명	
+	String unitPrice;		//가격
+	String description;		//상세 정보
+	String manufacturer;	//제조사
+	String category;		//분류
+	String unitsInStock;	//재고 수
+	String condition;		//상태
+	
+	
+	public String getProductid() {
+		return productid;
+	}
+	public void setProductid(String productid) {
+		this.productid = productid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(String unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getUnitsInStock() {
+		return unitsInStock;
+	}
+	public void setUnitsInStock(String unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+}
